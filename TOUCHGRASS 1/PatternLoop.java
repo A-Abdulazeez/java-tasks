67d0dfb86684a74ecdb37e081776e6bfe7462393
A_Abdulazeez <adebex999@gmail.com>
2026-04-20 00:57:51 +0100

    added

---
+++ TOUCHGRASS 1/PatternLoop.java
@@ -0,0 +1,19 @@
+public class PatternLoop{
+public static void main(String[]args){
+
+int reverseDigit = 8;
+while(reverseDigit > 1){
+reverseDigit--;
+
+System.out.print(reverseDigit);
+}
+
+for (int fowardDigit = 2;fowardDigit < 8;fowardDigit++){
+System.out.print(fowardDigit);
+}
+
+System.out.println();
+}
+}
+
+
